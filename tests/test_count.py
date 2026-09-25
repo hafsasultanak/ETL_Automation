a6d752479d81042afd1a1d63cb754ad4e74d1f4a
hafsasultanak <hafsasultanak@gmail.com>
2026-09-25 12:15:45 +0530

    updated test_count.py file

--- tests/test_count.py
+++ tests/test_count.py
@@ -3,3 +3,10 @@ def test_source_target_count():
     target_count = 100
 
     assert source_count == target_count
+
+def test_data_validation():
+    source_cust_id = [1,2,3,4,5,5]
+    target_cust_id = [1,2,3,4,5]
+
+    assert set (source_cust_id) == set(target_cust_id)
+
